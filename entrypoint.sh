9dffe41d7f2d3788a7832c37d9cae0f5bec24e43
sardncvsa <106793620+sardncvsa@users.noreply.github.com>
2023-09-29 00:42:16 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'cdfb700f-3316-4e6e-80b6-4256d5e76568'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
